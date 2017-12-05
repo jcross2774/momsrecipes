b3b35cfd9242814967871072e7644a4765076151
James Cross <jcross2774@gmail.com>
2017-12-04 22:47:14 -0700

    Make app producting ready with actioncable

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ end
 
 group :production do
   gem 'pg'
+  gem 'redis'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://jjc-momsrecipes.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://jjc-momsrecipes.herokuapp.com', 'http://jjc-momsrecipes.herokuapp.com']
 end
